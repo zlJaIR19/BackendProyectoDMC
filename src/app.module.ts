21cc28e97017a4d1783e1fd0c10385f11c9e1fe7
zlJaIR19 <d.jair.rivas.n@gmail.com>
2025-03-23 21:11:48 -0500

    agregar configuraciones de postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '1919',
+    database: 'tienda',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: false,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
