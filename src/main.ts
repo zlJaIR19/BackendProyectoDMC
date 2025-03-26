4599507d7b122e03dbc9346c04ef12ca9c732d20
zlJaIR19 <d.jair.rivas.n@gmail.com>
2025-03-26 02:15:20 -0500

    logica para front

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS for all origins
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
